2bec9aa8ce066d97c3bd5e311d8e2bcead7be7d8
unknown <ilya.bagrov.2004@gmail.com>
2023-03-27 16:33:27 +0500

    vot

---
+++ script.js
@@ -0,0 +1,36 @@
+// let name = prompt('Как вас зовут?').toLowerCase().trim()
+
+// let age = prompt('Сколько вам лет?').trim()
+
+// if(name === 'alex' && age >= 18) {
+//     alert('welcome')
+// } else {
+//     alert('no')
+// }
+
+
+// let a =  +prompt('Первое число')
+
+// let m = prompt('+ - * /')
+
+// let b = +prompt('Второе число')
+
+// if(m === '+') {
+//     alert(a + b)
+// } else if(m === '-') {
+//     alert(a - b)
+// } else if(m === '*') {
+//     alert(a * b)
+// } else if(m === '/') {
+//     alert(a / b)
+// }
+
+let number = +prompt('Любое число')
+
+if(number % 2 === 0) {
+    alert('Чётное')
+} else {
+    alert('Нечётное')
+}
+
+
